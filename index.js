3b03d3009498fdde1bfe87265836bc05f9a2b8a1
Md Nazmul Hossen <nazmulhossen.hstu.21@gmail.com>
2025-07-08 22:56:55 +0600

    Initialized my project server site by installing all needed packages and structured my project MVC pattern

---
+++ index.js
@@ -0,0 +1,34 @@
+// this is for root file
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+
+// import routers
+import { connectDB } from "./src/config/db.js";
+
+// starting
+const app = express();
+const port = process.env.port || 3000;
+
+// project middleware
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+// connect to the database
+connectDB();
+
+// api routes
+
+// root route
+
+app.get("/", (req, res) => {
+  res.send("My server is running ");
+});
+
+app.listen(port, () => {
+  console.log("server is running on port", port);
+});
+
+export default app;
---
+++ src/config/db.js
@@ -0,0 +1,32 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import "dotenv/config";
+
+const client = new MongoClient(process.env.MONGO_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// declared database collection
+
+let apartmentsCollection;
+
+const connectDB = async () => {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+
+    // declare of database and connect collection to the data base
+    const dataBase = client.db("My-House");
+    apartmentsCollection = dataBase.collection("apartments");
+  } catch (error) {
+    console.log("error from database connection", error);
+  }
+};
+
+export { apartmentsCollection, connectDB };
