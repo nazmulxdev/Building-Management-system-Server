0c9b5d1ae3dcc39958c2c6e68dc8ce0ee39e4b9c
Md Nazmul Hossen <nazmulhossen.hstu.21@gmail.com>
2025-07-12 18:53:11 +0600

    improved apartment controller to integrate pagination and filter implementation for minimum and maximum rent

--- src/api/controllers/apartment.controller.js
+++ src/api/controllers/apartment.controller.js
@@ -2,8 +2,38 @@ import { apartmentsCollection } from "../../config/db.js";
 
 const getAllApartments = async (req, res) => {
   try {
-    const apartments = await apartmentsCollection.find().toArray();
-    res.send(apartments);
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 6;
+    const skip = (page - 1) * limit;
+    const minRent = parseInt(req.query.minRent);
+    const maxRent = parseInt(req.query.maxRent);
+
+    // building the query object
+    const query = {};
+    if (!isNaN(minRent) && !isNaN(maxRent)) {
+      query.rent = { $gte: minRent, $lte: maxRent };
+    } else if (!isNaN(minRent)) {
+      query.rent = { $gte: minRent };
+    } else if (!isNaN(maxRent)) {
+      query.rent = { $lte: maxRent };
+    }
+
+    // getting total number of apartment
+    const totalApartments = await apartmentsCollection.countDocuments(query);
+    
+    // calculation of total page
+    const totalPages = Math.ceil(totalApartments / limit);
+    const apartments = await apartmentsCollection
+      .find(query)
+      .skip(skip)
+      .limit(limit)
+      .toArray();
+    res.send({
+      apartments,
+      totalPages,
+      currentPage: page,
+      totalApartments,
+    });
   } catch (error) {
     res.status(500).send({ message: "Failed to load apartments details" });
   }
